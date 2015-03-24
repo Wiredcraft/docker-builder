9938b7dc269e70c8dcad6d1b8985cb221b936ba6
kuno <kuno.the.developer@gmail.com>
2015-03-24 14:14:52 +0800

    Add test for get_image_id

--- tests/test_docker_builder.py
+++ tests/test_docker_builder.py
@@ -16,6 +16,27 @@ empty_fd = os.open(empty_file_path, os.O_RDONLY)
 from docker_builder import __version__
 from docker_builder import LineReader
 from docker_builder import Builder
+from docker_builder import get_image_id
+
+
+# http://stackoverflow.com/questions/377017/test-if-executable-exists-in-python
+def which(program):
+
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
 
 # Test
 assert __version__ == '0.2'
@@ -40,4 +61,14 @@ assert empty_builder.config is None
 assert valid_builder.config is not None
 assert type(valid_builder.config) is dict
 
-# TODO: more tests
+# Test
+empty_image_id = get_image_id('')
+valid_image_id = get_image_id('Successfully built' + ' 123')
+assert empty_image_id is False
+assert valid_image_id == '123'
+
+#
+if which('docker'):
+    assert True
+else:
+    pass
